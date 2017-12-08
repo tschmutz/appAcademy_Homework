d92a134b538f60cb374e9e08c45c1a7c26c0d54d
Taylor Schmutz <taylorschmutz@gmail.com>
2017-12-07 21:48:58 -0800

    Add stack class

---
+++ w1d5/exercises.rb
@@ -0,0 +1,31 @@
+class Stack
+
+  def initialize
+    @stack = []
+  end
+
+  def add(el)
+    stack << el
+  end
+
+  def remove
+    stack.pop
+  end
+
+  def show
+    stack
+  end
+
+end
+
+
+class queue
+
+  def initialize
+    @stack = stack
+  end
+
+  
+
+
+end
