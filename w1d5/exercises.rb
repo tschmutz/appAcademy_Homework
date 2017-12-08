396b6becebbd0f04bb2bcf3e6e406385b25a4fa4
Taylor Schmutz <taylorschmutz@gmail.com>
2017-12-07 22:24:03 -0800

    Add map class

--- w1d5/exercises.rb
+++ w1d5/exercises.rb
@@ -19,7 +19,7 @@ class Stack
 end
 
 
-class queue
+class Queue
 
   def initialize
     @stack = []
@@ -36,4 +36,33 @@ class queue
   def show
     stack
   end
+end
+
+class Map
+
+  def initialize
+    @map = []
+  end
+
+   def assign(key, value)
+     if lookup(key)
+       @map[lookup(key)] = [key, value]
+     else
+       @map << [key, value]
+     end
+     @map 
+   end
+
+   def remove(key)
+     @map.select { |keyval| keyval[0] != key}
+   end
+
+   def lookup(key)
+     @map.each_with_index { |pair, idx| return idx if pair[0] == key}
+     nil
+   end
+
+   def show
+     @map
+   end
 end
