15edbce13448651655574fda601774473eadc392
Taylor Schmutz <taylorschmutz@gmail.com>
2017-12-12 08:54:27 -0800

    Add mancala

---
+++ mancala/lib/board.rb
@@ -0,0 +1,77 @@
+class Board
+  attr_accessor :cups
+
+  def initialize(name1, name2)
+    @cups = Array.new(14) {Array.new}
+    place_stones
+  end
+
+  def place_stones
+    # helper method to #initialize every non-store cup with four stones each
+    @cups.each_with_index do |cup, i|
+      next if i == 6 || i == 13
+      4.times do
+        cup << :stone
+      end
+    end
+  end
+
+  def valid_move?(start_pos)
+    if start_pos < 0 || start_pos > 12 || @cups[start_pos].empty?
+      raise "Invalid starting cup"
+    end
+  end
+
+  def make_move(start_pos, current_player_name)
+    stones = @cups[start_pos]
+    @cups[start_pos] = []
+    idx = start_pos
+    until stones.empty?
+      idx += 1
+      idx = 0 if idx > 13
+      if idx == 6
+        @cups[6] << stones.pop if current_player_name == @name1
+      elsif idx == 13
+        @cups[13] << stones.pop if current_player_name == @name2
+      else
+        @cups[idx] << stones.pop
+      end
+    end
+    render
+    next_turn(idx)
+  end
+
+  def next_turn(ending_cup_idx)
+    # helper method to determine what #make_move returns
+    if ending_cup_idx == 6 || ending_cup_idx == 13
+      :prompt
+    elsif @cups[ending_cup_idx].count == 1
+      :switch
+    else
+      ending_cup_idx
+    end
+  end
+
+  def render
+    print "      #{@cups[7..12].reverse.map { |cup| cup.count }}      \n"
+    puts "#{@cups[13].count} -------------------------- #{@cups[6].count}"
+    print "      #{@cups.take(6).map { |cup| cup.count }}      \n"
+    puts ""
+    puts ""
+  end
+
+  def one_side_empty?
+    @cups[0..6].all? { |cup| cup.empty?} || @cups[7..13].all? { |cup| cup.empty?}
+  end
+
+  def winner
+    player1_count = @cups[6].count
+    player2_count = @cups[13].count
+
+    if player1_count == player2_count
+      :draw
+    else
+      player1_count > player2_count ? @name1 : @name2
+    end
+  end
+end
