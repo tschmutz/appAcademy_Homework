a49525e692a1e5096a5358de1864ff740b43e460
Taylor Schmutz <taylorschmutz@gmail.com>
2018-01-21 18:26:10 -0800

    Add store and reducer

---
+++ w7d1/dollar_store/frontend/actions.js
@@ -0,0 +1,13 @@
+
+
+const selectCurrency = (baseCurrency, rates) => {
+  
+  return {
+    type: 'SWITCH_CURRENCY',
+    baseCurrency: baseCurrency,
+    rates: rates 
+  };
+};
+
+
+export default selectCurrency;
